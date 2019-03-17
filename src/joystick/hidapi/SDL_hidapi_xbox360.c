9fe9bed54672a700b8a7e8882be14d267f3656d8
Richard Smith <mercurial@fantastic.me.uk>
2019-03-17 01:50:37 +0000

    fix gcc compiler warnings

--- src/joystick/hidapi/SDL_hidapi_xbox360.c
+++ src/joystick/hidapi/SDL_hidapi_xbox360.c
@@ -717,7 +717,7 @@ HIDAPI_DriverXbox360_HandleStatePacket(SDL_Joystick *joystick, hid_device *dev, 
     /* Update match_state with button bit, then fall through */
 #   define SDL_PrivateJoystickButton(joystick, button, state) if (state) match_state |= 1 << (button); else match_state &=~(1<<(button)); SDL_PrivateJoystickButton(joystick, button, state)
     /* Grab high 4 bits of value, then fall through */
-#   define SDL_PrivateJoystickAxis(joystick, axis, value) if (axis < 4) match_state = match_state & ~(0xF << (4 * axis + 16)) | ((value) & 0xF000) << (4 * axis + 4); SDL_PrivateJoystickAxis(joystick, axis, value)
+#   define SDL_PrivateJoystickAxis(joystick, axis, value) if (axis < 4) match_state = (match_state & ~(0xF << (4 * axis + 16))) | ((value) & 0xF000) << (4 * axis + 4); SDL_PrivateJoystickAxis(joystick, axis, value)
 #endif
     Sint16 axis;
     SDL_bool has_trigger_data = SDL_FALSE;
@@ -830,12 +830,14 @@ HIDAPI_DriverXbox360_HandleStatePacket(SDL_Joystick *joystick, hid_device *dev, 
     SDL_memcpy(ctx->last_state, data, SDL_min(size, sizeof(ctx->last_state)));
 }
 
+#ifdef SDL_JOYSTICK_RAWINPUT
 static void
 HIDAPI_DriverXbox360_HandleStatePacketFromRAWINPUT(SDL_Joystick *joystick, void *context, Uint8 *data, int size)
 {
     SDL_DriverXbox360_Context *ctx = (SDL_DriverXbox360_Context *)context;
     HIDAPI_DriverXbox360_HandleStatePacket(joystick, NULL, ctx, data, size);
 }
+#endif
 
 #else
 
@@ -999,6 +1001,9 @@ HIDAPI_DriverXbox360_Update(SDL_Joystick *joystick, hid_device *dev, void *conte
     SDL_DriverXbox360_Context *ctx = (SDL_DriverXbox360_Context *)context;
     Uint8 data[USB_PACKET_LENGTH];
     int size = 0;
+    SDL_bool has_trigger_data = SDL_FALSE;
+    SDL_bool correlated = SDL_FALSE;
+    WindowsMatchState match_state_xinput;
 
     while (dev && (size = hid_read_timeout(dev, data, sizeof(data), 0)) > 0) {
 #ifdef __WIN32__
@@ -1037,7 +1042,6 @@ HIDAPI_DriverXbox360_Update(SDL_Joystick *joystick, hid_device *dev, void *conte
 
     /* Poll for trigger data once (not per-state-packet) */
 #ifdef SDL_JOYSTICK_HIDAPI_WINDOWS_XINPUT
-    SDL_bool has_trigger_data = SDL_FALSE;
     /* Prefer XInput over WindowsGamingInput, it continues to provide data in the background */
     if (!has_trigger_data && ctx->xinput_enabled && ctx->xinput_correlated) {
         HIDAPI_DriverXbox360_UpdateXInput();
@@ -1064,9 +1068,6 @@ HIDAPI_DriverXbox360_Update(SDL_Joystick *joystick, hid_device *dev, void *conte
 #endif /* SDL_JOYSTICK_HIDAPI_WINDOWS_GAMING_INPUT */
 
 #ifdef SDL_JOYSTICK_HIDAPI_WINDOWS_MATCHING
-    SDL_bool correlated = SDL_FALSE;
-
-    WindowsMatchState match_state_xinput;
     HIDAPI_DriverXbox360_FillMatchState(&match_state_xinput, ctx->match_state);
 
 #ifdef SDL_JOYSTICK_HIDAPI_WINDOWS_GAMING_INPUT
@@ -1246,10 +1247,10 @@ HIDAPI_DriverXbox360_Update(SDL_Joystick *joystick, hid_device *dev, void *conte
 
     if (!correlated) {
         if (!guide_button_candidate.joystick ||
-            ctx->last_state_packet && (
+            (ctx->last_state_packet && (
                 !guide_button_candidate.last_state_packet ||
                 SDL_TICKS_PASSED(ctx->last_state_packet, guide_button_candidate.last_state_packet)
-            )
+            ))
         ) {
             guide_button_candidate.joystick = joystick;
             guide_button_candidate.last_state_packet = ctx->last_state_packet;
